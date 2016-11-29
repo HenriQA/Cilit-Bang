68d3d814a6141de36a93565ac9a750a558b7a941
Michael Brewster <michael.brewster@qa.com>
2016-11-29 16:24:30 +0000

    git module

--- shared_master/modules/git/manifests/init.pp
+++ shared_master/modules/git/manifests/init.pp
@@ -1,6 +1,10 @@
 class git
 {
-	exec { 'install git':
-		command => 'sudo apt-get install -y git',
+	Exec { 
+		path => ["/usr/bin", "/bin", "/usr/sbin"]
+	}
+		
+		exec{ 'install git':
+			command => 'sudo apt-get install -y git',
 		}
 }
