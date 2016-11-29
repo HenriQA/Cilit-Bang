6968cb5d552f36df5400ebdda19f34b57d2626bf
HenriQA <henri.william@qa.com>
2016-11-29 14:47:33 +0000

    Update and rename Java.txt to java_init.pp

--- java_init.pp
+++ java_init.pp
@@ -1,5 +1,5 @@
 class java (
-$java_archive = 'java.tar.gz",
+$java_archive = 'java.tar.gz',
 $java_home = '/opt/jdk1.8.0_45',
 $java_folder = 'jdk1.8.0_45',)
 
@@ -8,7 +8,7 @@ Exec {
 path => [ '/usr/bin', 'usr/sbin', '/bin'],
 }
 
-file { "/opt/${java_archive}":
+file { "/opt/${java_archive}" :
 ensure => present,
 source => "puppet:///modules/java/${java_archive}",
 owner => vagrant,
@@ -19,7 +19,7 @@ exec {'extract jdk':
 cwd => '/opt/',
 command => "sudo tar zxfv ${java_archive}",
 creates => '$java_home',
-# require => File['/opt/${java_archive}']
+require => File["/opt/${java_archive}"]
 }
 
 exec { 'install java':
@@ -29,15 +29,15 @@ command => 'update-alternatives --install usr/bin/java java ${java_home}/bin/jav
 }
 
 exec {'install javac':
-require => Exec['extract java'],
+require => Exec['extract jdk'],
 logoutput => true,
 command => 'update-alternatives --install /bin/javac javac ${java_home}/bin/javac 100',
 }
 
-file { '/etc.profile.d/java/sh':
-content => 'export JAVA_HOME=${java_home}
-            export PATH=\$PATH:\$JAVA_HOME/bin'
-			}
+#file { '/etc.profile.d/java/sh':
+#content => "export JAVA_HOME=${java_home}
+#            export PATH=\$PATH:\$JAVA_HOME/bin"
+#			}
 			
 }
 
